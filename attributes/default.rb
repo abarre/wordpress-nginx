9f8f045e6d66c814943eeb5f1a84eb7c3674c7ca
abarre <anthony.barre87@gmail.com>
2014-03-22 22:37:55 +0100

    Fix the cookbook name

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['wordpress_nginx']['fastcgi_read_timeout'] = '3600s'
 default['wordpress_nginx']['static_file_expiry'] = '24h'
 default['wordpress_nginx']['php_fpm']['listen'] = '/var/run/php-fpm-www.sock'
 default['wordpress_nginx']['listen'] = "80"
-default['wordpress-nginx']['cookbook'] = nil
+default['wordpress-nginx']['cookbook'] = "wordpress-nginx"
