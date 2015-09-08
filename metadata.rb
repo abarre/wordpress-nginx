c9b94e9a424960d0309496e3842540d9bb9299c5
abarre <anthony.barre87@gmail.com>
2015-09-08 22:58:23 +0200

    Update to 0.3.2.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'scott.walkinshaw@gmail.com'
 license          'MIT'
 description      'Installs/Configures Nginx sites for WordPress with PHP-FPM'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.1'
+version          '0.3.2'
 
 depends          'nginx'
 depends          'php-fpm'
